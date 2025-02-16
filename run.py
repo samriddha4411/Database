8fb33fef884d136aefedf36d971b72714026ffde
cyberdeep-corp <dev.samworks@gmail.com>
2025-02-16 11:37:37 +0000

    Second Main v0.1.1

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import *
-
-import socket
-
-print(socket.gethostname())
-print(socket.gethostbyname(socket.gethostname()))
-
-app.run(debug=True, host=socket.gethostbyname(socket.gethostname()), port=80)
